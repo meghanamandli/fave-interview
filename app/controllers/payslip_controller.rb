06f14407dbfb4c6a95b838bf5fbc765aca37be99
Meghana Mandli <mmandli@groupon.com>
2023-02-24 23:06:37 +0530

    added initial changes for requirement 2 branch.

--- app/controllers/payslip_controller.rb
+++ app/controllers/payslip_controller.rb
@@ -1,10 +1,19 @@
 class PayslipController < ApplicationController
-  
+
+  skip_before_action :verify_authenticity_token
   include TaxBracketHelper
 
-  def generate_monthly_payslip(income, name)
+  def get_payslip_information
+  	@name = params[:name]
+  	@income = params[:income].to_i
+  	generate_monthly_payslip(@name, @income)
+  end
+
+  def generate_monthly_payslip(name, income)
+  	binding.pry
     @income = income
-    @tax_bracket = TaxBracketHelper.get_tax_information
+    @tax_bracket = view_context.get_tax_information
+    binding.pry
     @available_range = @tax_bracket.select {|income| income === @income}
     @id = @available_range.first[1][:id]
     @bracket = @tax_bracket.first(@id).to_h
@@ -22,7 +31,12 @@ class PayslipController < ApplicationController
 
 
   def get_formatted_value(tax, name, income)
+  	if params.present?
+    payslip_data = {"employee_name" => name, "gross_monthly_income" => @income/12, "monthly_income_tax" => @tax/12, "net_monthly_income" => (@income/12 - @tax/12) }
+    render :json => payslip_data
+  	else
   	payslip_data = {"Monthly Payslip for" => name, "Gross Monthly Income:" => @income/12, "Monthly Income Tax:" => @tax/12, "Net Monthly Income:" => (@income/12 - @tax/12) }
-    return :json => payslip_data
+  	return :json => payslip_data
+  	end
   end
 end
